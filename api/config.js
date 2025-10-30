1917d059dc1bc87303ea6cf59dae18a7ac6649e3
gagaf7 <145220548+gagaf7@users.noreply.github.com>
2025-10-30 15:50:29 +0100

    update

--- api/config.js
+++ api/config.js
@@ -1,11 +1,11 @@
 module.exports =  {
     ACCESS_TOKEN_SECRET : "EMMA123",
     BDD : {
-    "host" :"dpg-d40798bipnbc73cf4njg-a.oregon-postgres.render.com",
+    "host" :"dpg-d41mvajipnbc73fh9frg-a.oregon-postgres.render.com",
     "port" : "5432",
-    "user" : "cnam_qnjc_user",
-    "password" : "FOYjmLxOmqwdnfGPhMO4R8eZpErPzVXQ",
-    "bdname" :"cnam_qnjc" 
+    "user" : "pollutionbase_user",
+    "password" : "7EQ3friduwflBFcYH1H0K5lmKEeFcyTg",
+    "bdname" :"pollutionbase" 
     }
 }
 
