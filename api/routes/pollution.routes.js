471890ae56e51b086c1d03adbc7c6dd294549ce6
gagaf7 <145220548+gagaf7@users.noreply.github.com>
2025-11-11 17:30:49 +0100

    estt

--- api/routes/pollution.routes.js
+++ api/routes/pollution.routes.js
@@ -1,14 +1,13 @@
+
+
 module.exports = app => {
-  const pollutionController = require("../controllers/pollution.controllers.js");
+    const pollution = require("../controllers/pollution.controllers.js");
+  
+    var router = require("express").Router();
+  
 
-  var router = require("express").Router();
-
-  router.post("/", pollutionController.create);
-  router.get("/", pollutionController.findAll);
-  router.get("/:id", pollutionController.findOne);
-  router.put("/:id", pollutionController.update);
-  router.delete("/:id", pollutionController.delete);
-  router.delete("/", pollutionController.deleteAll);
-
-  app.use('/api/pollutions', router);
-};
+   
+    router.get("/", pollution.get);
+  
+    app.use('/api/pollution', router);
+  };
--- api/routes/utilisateur.routes.js
+++ api/routes/utilisateur.routes.js
@@ -1,15 +1,12 @@
+
 module.exports = app => {
-  const utilisateurController = require("../controllers/utilisateur.controllers.js");
+    const pollution = require("../controllers/utilisateur.controllers.js");
+  
+    var router = require("express").Router();
+  
 
-  var router = require("express").Router();
-
-  router.post("/", utilisateurController.create);
-  router.get("/", utilisateurController.findAll);
-  router.get("/:id", utilisateurController.findOne);
-  router.put("/:id", utilisateurController.update);
-  router.delete("/:id", utilisateurController.delete);
-  router.post("/login", utilisateurController.login);
-  router.post("/signup", utilisateurController.signup);
-
-  app.use('/api/users', router);
-};
+   
+    router.get("/", pollution.get);
+  
+    app.use('/api/pollution', router);
+  };
