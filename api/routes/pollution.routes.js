3d89b49e6919f4af61ce72c55beab5c5b4c715de
gagaf7 <145220548+gagaf7@users.noreply.github.com>
2025-11-11 17:37:07 +0100

    update

--- api/routes/pollution.routes.js
+++ api/routes/pollution.routes.js
@@ -1,13 +1,14 @@
-
-
 module.exports = app => {
-    const pollution = require("../controllers/pollution.controllers.js");
-  
-    var router = require("express").Router();
-  
+  const pollutionController = require("../controllers/pollution.controllers.js");
 
-   
-    router.get("/", pollution.get);
-  
-    app.use('/api/pollution', router);
-  };
+  var router = require("express").Router();
+
+  router.post("/", pollutionController.create);
+  router.get("/", pollutionController.findAll);
+  router.get("/:id", pollutionController.findOne);
+  router.put("/:id", pollutionController.update);
+  router.delete("/:id", pollutionController.delete);
+  router.delete("/", pollutionController.deleteAll);
+
+  app.use('/api/pollutions', router);
+};
