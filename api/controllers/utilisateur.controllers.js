482cac1b8f0e4f5a5c0c2b5d4a1ce97195a31b72
gagaf7 <145220548+gagaf7@users.noreply.github.com>
2025-11-11 16:29:17 +0100

    update test

--- api/controllers/utilisateur.controllers.js
+++ api/controllers/utilisateur.controllers.js
@@ -1,44 +1,88 @@
-const { v4: uuidv4 } = require ("uuid");
-
-
 const db = require("../models");
-const Utilisateurs = db.utilisateurs;
-const Op = db.Sequelize.Op;
+const Utilisateur = db.utilisateur;
 
-// Find a single Utilisateur with an login
-exports.login = (req, res) => {
+// Get all Utilisateurs
+exports.get = (req, res) => {
+  Utilisateur.findAll()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving utilisateurs."
+      });
+    });
+};
+
+// Sign Up - Create a new Utilisateur
+exports.signup = (req, res) => {
+  if (!req.body.email || !req.body.password || !req.body.username) {
+    res.status(400).send({
+      message: "Email, password and username are required!"
+    });
+    return;
+  }
+
   const utilisateur = {
-    login: req.body.login,
+    username: req.body.username,
+    email: req.body.email,
     password: req.body.password
   };
 
-  // Test
-  let pattern = /^[A-Za-z0-9]{1,20}$/;
-  if (pattern.test(utilisateur.login) && pattern.test(utilisateur.password)) {
-     Utilisateurs.findOne({ where: { login: utilisateur.login } })
+  Utilisateur.create(utilisateur)
     .then(data => {
-      if (data) {
-        const user = {
-          id: data.id,
-          name: data.nom,
-          email: data.email
-        };
-      
-        res.send(data);
-      } else {
+      res.send({
+        message: "Utilisateur created successfully!",
+        utilisateur: data
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the utilisateur."
+      });
+    });
+};
+
+// Login
+exports.login = (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    res.status(400).send({
+      message: "Email and password are required!"
+    });
+    return;
+  }
+
+  Utilisateur.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    .then(data => {
+      if (!data) {
         res.status(404).send({
-          message: `Cannot find Utilisateur with login=${utilisateur.login}.`
+          message: "Utilisateur not found!"
+        });
+      } else if (data.password !== req.body.password) {
+        res.status(401).send({
+          message: "Invalid password!"
+        });
+      } else {
+        res.send({
+          message: "Login successful!",
+          utilisateur: {
+            id: data.id,
+            username: data.username,
+            email: data.email
+          }
         });
       }
     })
     .catch(err => {
-      res.status(400).send({
-        message: "Error retrieving Utilisateur with login=" + utilisateur.login
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while logging in."
       });
     });
-  } else {
-    res.status(400).send({
-      message: "Login ou password incorrect" 
-    });
-  }
 };
--- api/routes/pollution.routes.js
+++ api/routes/pollution.routes.js
@@ -1,13 +1,14 @@
 module.exports = app => {
-    const pollutionController = require("../controllers/pollution.controllers.js");
-  
-    var router = require("express").Router();
-  
-    router.get("/", pollutionController.get);
-    router.post("/", pollutionController.create);
-    router.get("/:id", pollutionController.getById);
-    router.put("/:id", pollutionController.update);
-    router.delete("/:id", pollutionController.delete);
-  
-    app.use('/api/pollutions', router);
+  const pollutionController = require("../controllers/pollution.controllers.js");
+
+  var router = require("express").Router();
+
+  router.post("/", pollutionController.create);
+  router.get("/", pollutionController.get);
+  router.get("/:id", pollutionController.getById);
+  router.put("/:id", pollutionController.update);
+  router.delete("/:id", pollutionController.delete);
+  router.delete("/", pollutionController.deleteAll);
+
+  app.use('/api/pollutions', router);
 };
